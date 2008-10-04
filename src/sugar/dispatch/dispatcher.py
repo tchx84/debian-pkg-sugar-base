405dc0bf339939bb2468c63910c16dad35f95710
Marco Pesenti Gritti <mpgritti@gmail.com>
2008-10-04 03:50:00 +0200

    Add django license. Fix django imports.

--- src/sugar/dispatch/dispatcher.py
+++ src/sugar/dispatch/dispatcher.py
@@ -4,7 +4,7 @@ try:
 except NameError:
     from sets import Set as set # Python 2.3 fallback
 
-from django.dispatch import saferef
+from sugar.dispatch import saferef
 
 WEAKREF_TYPES = (weakref.ReferenceType, saferef.BoundMethodWeakref)
 
@@ -60,30 +60,7 @@ class Signal(object):
             may be anything hashable.
 
         returns None
-        """
-        from django.conf import settings
-        
-        # If DEBUG is on, check that we got a good receiver
-        if settings.DEBUG:
-            import inspect
-            assert callable(receiver), "Signal receivers must be callable."
-            
-            # Check for **kwargs
-            # Not all callables are inspectable with getargspec, so we'll
-            # try a couple different ways but in the end fall back on assuming
-            # it is -- we don't want to prevent registration of valid but weird
-            # callables.
-            try:
-                argspec = inspect.getargspec(receiver)
-            except TypeError:
-                try:
-                    argspec = inspect.getargspec(receiver.__call__)
-                except (TypeError, AttributeError):
-                    argspec = None
-            if argspec:
-                assert argspec[2] is not None, \
-                    "Signal receivers must accept keyword arguments (**kwargs)."
-        
+        """       
         if dispatch_uid:
             lookup_key = (dispatch_uid, _make_id(sender))
         else:
