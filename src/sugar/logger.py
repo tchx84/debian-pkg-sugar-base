7a90d31e8401c74126a4d811a547a36e4c6ee75b
Marco Pesenti Gritti <mpgritti@gmail.com>
2008-10-28 14:18:25 +0100

    Mark modules as STABLE.

--- src/sugar/logger.py
+++ src/sugar/logger.py
@@ -15,6 +15,11 @@
 # Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 # Boston, MA 02111-1307, USA.
 
+"""Logging service setup.
+
+STABLE.
+"""
+
 import errno
 import sys
 import os
--- src/sugar/mime.py
+++ src/sugar/mime.py
@@ -16,6 +16,11 @@
 # Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 # Boston, MA 02111-1307, USA.
 
+"""MIME helpers based on freedesktop specification.
+
+STABLE.
+"""
+
 import os
 import logging
 import gettext
