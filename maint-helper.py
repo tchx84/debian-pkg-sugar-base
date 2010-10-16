1be25d011be856be1655f3518f9afecc77ce1e50
Sascha Silbe <sascha-pgp@silbe.org>
2010-10-16 08:45:16 +0000

    fix EOL spaces
    Reviewed-by: James Cameron <quozl@laptop.org>
    Acked-by: Simon Schampijer <simon@laptop.org>

--- maint-helper.py
+++ maint-helper.py
@@ -16,7 +16,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-# Latest source available at git://dev.laptop.org/sugar 
+# Latest source available at git://dev.laptop.org/sugar
 
 import os
 import sys
