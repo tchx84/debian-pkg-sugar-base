c9498bb5ee718fe741321a94088a6cab11df4824
Daniel Narvaez <dwnarvaez@gmail.com>
2012-06-26 13:06:18 +0000

    Allow to build outside source directory
    Reviewed-by: James Cameron <quozl@laptop.org>
    Acked-by: Simon Schampijer <simon@schampijer.de>

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
+
+test -n "${srcdir}" || srcdir=`dirname "$0"`
+test -n "${srcdir}" || srcdir="$(pwd)"
+
+olddir="$(pwd)"
+cd "$srcdir"
+
 export ACLOCAL="aclocal -I m4"
 
 intltoolize
 autoreconf -i
-./configure "$@"
+
+cd "$olddir"
+"$srcdir/configure" "$@"
